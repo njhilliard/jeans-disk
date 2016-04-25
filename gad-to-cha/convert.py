a02cf9dc114f1ada6bad43e6fa6905f7fdf52f30
Nate Hilliard <phaionix@users.noreply.github.com>
2016-04-24 20:20:24 -0500

    Finished reworking conversion. New conversion dictionary where ordering matters. Needs user input yet.

--- gad-to-cha/convert.py
+++ gad-to-cha/convert.py
@@ -5,7 +5,10 @@
 
 import sys
 import numpy as np
+import math
+from collections import OrderedDict
 from astropy import units as u
+from astropy.constants import G
 from param_dict import g2c_name_dict
 
 class convert_g2c():
@@ -19,114 +22,88 @@ class convert_g2c():
         self.empt_idx = []
         self.inlines  = []
         self.outlines = []
-        self.gad_dict = {}
-        self.cha_dict = {}
+        self.gad_dict = OrderedDict()
+        self.cha_dict = OrderedDict()
 
     def read(self, f_name):
         """Read gadget parameter file"""
         with open(f_name, 'r') as f:
             self.inlines = f.readlines()
 
-    def parse(self):
+    def parse(self, g2c_name_dict):
         """Parse the gadget parameter file"""
-        for i,line in enumerate(self.inlines):
+        for line in self.inlines:
             line.rstrip('\n')
-            #Remove comments
-            if '%' in list(line)[0]:
-                #changa prms comments begin with pound
-                self.comments.append('#' + line)
-                self.comm_idx.append(i) #store comment indices
-            #elif '%' in list(line):   #remove endline comments
-                #lines[i] = line.split('%',1)[0]
-            # remove empty lines or lines beginning with space
-            elif ' ' in list(line)[0] or len(line)==1:
-                self.empt_idx.append(i)
-            else:
+            if not '%' in list(line)[0] and not ' ' in list(line)[0]\
+                                        and not len(line)==1:
                 linesplit = line.split()
-                self.gad_name.append(linesplit[0])
                 self.gad_dict[linesplit[0]] = linesplit[1]
-                self.prms_idx.append(i)
         for key in g2c_name_dict:
             if g2c_name_dict[key] != '':
                 self.cha_dict[g2c_name_dict[key]] = self.gad_dict[key]
+
         return self.gad_dict, self.cha_dict
 
-    def build_dict(self, f_name, d_name):
-        """Build a conversion dictionary from a gadget params file.
-           Currently untested."""
-        with open(dict_name, 'w') as f:
-            #f.write('# gadget.param : changa.param\n')
-            f.write(d_name + ' = {\\\n')
-            for prm_name in self.gdt_name:
-                f.write('    {:40}{}'.format(\
-                        '\'' + prm_name + '\'',':\'\',\\'))
-                f.write('\n')
-            f.write('        }')
-
-    def build_outlines(self, g2c_name_dict):
+    def build_outlines(self):
         """Write output changa parameter file"""
-        comm_ct = 0
-        prms_ct = 0
-        for i in range(len(self.inlines)):
-            if i in self.prms_idx:
-                if g2c_name_dict[self.gad_name[prms_ct]] == '':
-                    pass
-                else:
-                    hold2 = [g2c_name_dict[self.gad_name[prms_ct]],\
-                             self.gad_dict[self.gad_name[prms_ct]]]
-                    self.outlines.append(hold2)
-                prms_ct += 1
-            # Add comment lines
-            elif i in self.comm_idx:
-                #self.outlines.append(self.comments[comm_ct])
-                comm_ct += 1
-            elif i in self.empt_idx: # Add blank lines
-                #self.outlines.append('') 
-                pass
-            # Add nonempty dict matches and converted vals
-            else: # Tell when something doesn't work
-                print('Error: unknown line passed to build_outlines')
-                self.outlines.append('badline')
-
-        # remove lines after last parameter 
-        # (may be better to do this earlier)
-        for i,line in enumerate(reversed(self.outlines)):
-            if type(line) == list:
-                last_prm_idx = len(self.outlines) - i
-                break
-            else:
-                pass
-        self.outlines = self.outlines[:last_prm_idx]
+        for key in self.cha_dict:
+            self.outlines.append([key, self.cha_dict[key]])
         
         return self.outlines
 
     def convert_vals(self):
         """Convert parameter values"""
         # mash together gadget directory output and file prefix
-        self.gad_dict['SnapshotFileBase'] =\
+        self.cha_dict['achOutFile'] =\
                 self.gad_dict['OutputDir'] + '/'\
-                + self.gad_dict['OutputDir']
+                + self.gad_dict['SnapshotFileBase']
         # wall runtime limit seconds to minutes
-        self.gad_dict['TimeLimitCPU'] =\
+        self.cha_dict['iWallRunTime'] =\
                 str(int(self.gad_dict['TimeLimitCPU'])/ 60)
-        #self.gad_dict['TimeBetSnapshot'] =\
-                #np.ceil(self.gad_dict['TimeMax'])
-                
-        # timesteps
-        #####self.gad_dict['TimeBetSnapshot'] =\
+        # simulation start step
+        if self.gad_dict['TimeBegin'] != 0:
+            step_floor = math.floor(float(self.gad_dict['TimeBegin'])/\
+                                    float(self.cha_dict['dDelta']))
+            self.cha_dict['iStartStep'] = str(step_floor)
+        else:
+            self.cha_dict['iStartStep'] = '0'
+        # number of simulation steps
+        self.cha_dict['nSteps'] = str(math.ceil(\
+                (float(self.gad_dict['TimeMax'])\
+                 - float(self.gad_dict['TimeBegin']))\
+                / float(self.cha_dict['dDelta'])))
+        # output writing interval
+        self.cha_dict['iOutInterval'] = str(math.ceil(\
+                float(self.gad_dict['TimeBetSnapshot'])\
+                / float(self.cha_dict['dDelta'])))
+        # output to log file interval
+        self.cha_dict['iLogInterval'] = str(math.floor(\
+                float(self.gad_dict['TimeBetStatistics'])\
+                / float(self.cha_dict['dDelta'])))
+        # factor difference for Eta parameter
+        factor = 1 # placeholder
+        self.cha_dict['dEta'] =\
+                str(factor * float(self.gad_dict['ErrTolIntAccuracy']))
         # gadget courant factor is half of normal
-        self.gad_dict['CourantFac'] =\
+        self.cha_dict['dEtaCourant'] =\
                 str(2 * float(self.gad_dict['CourantFac']))
         # convert cm to kpc
         unitlength = float(self.gad_dict['UnitLength_in_cm']) * u.cm
-        self.gad_dict['UnitLength_in_cm'] =\
-                str(float(unitlength.to(u.kpc) / u.kpc))
+        dKpcUnit = float(unitlength.to(u.kpc) / u.kpc)
+        self.cha_dict['dKpcUnit'] = str(dKpcUnit)
         # convert g to solar masses
         unitmass = float(self.gad_dict['UnitMass_in_g']) * u.g
-        self.gad_dict['UnitMass_in_g'] =\
-                str(float(unitmass.to(u.solMass) / u.solMass))
+        unitvelocity = float(self.gad_dict['UnitVelocity_in_cm_per_s'])\
+                       * u.cm / u.s
+        dMsolUnit =float(  ( ( ((dKpcUnit * u.kpc).to(u.m))**3\
+                              / (unitlength / unitvelocity)**2\
+                             ) / G\
+                           ).to(u.Msun) / u.Msun\
+                        )
+        self.cha_dict['dMsolUnit'] = str(dMsolUnit)
+        print('Warning: mass of simulation has changed due to changa G=1')
 
-        return self.gad_dict
+        return self.cha_dict
 
     def write(self, f_name):
         """Write output changa parameter file"""
@@ -143,14 +120,13 @@ class convert_g2c():
 
 if __name__ == "__main__":
     fopen = 'gadget.params'
-    fwrite = 'changa.params'
+    fwrite = 'changa.param'
     convert = convert_g2c()
     convert.read(fopen)
-    #print(convert.parse())
     #print(convert.build_outlines(g2c_name_dict))
-    convert.parse()
+    convert.parse(g2c_name_dict)
     #print(convert.convert_vals())
     convert.convert_vals()
-    convert.build_outlines(g2c_name_dict)
+    convert.build_outlines()
     convert.write(fwrite)
 
--- gad-to-cha/param_dict.py
+++ gad-to-cha/param_dict.py
@@ -1,84 +1,84 @@
+from collections import OrderedDict
+g2c_name_dict = OrderedDict()
 # gadget.param : changa.param
-g2c_name_dict = {\
-    'CritPhysDensity'               :'',\
-    'MaxSfrTimescale'               :'',\
-    'CritOverDensity'               :'',\
-    'TempSupernova'                 :'',\
-    'TempClouds'                    :'',\
-    'FactorSN'                      :'',\
-    'FactorEVP'                     :'',\
-    'WindEfficiency'                :'',\
-    'WindEnergyFraction'            :'',\
-    'WindFreeTravelDensFac'         :'',\
-    'WindFreeTravelMaxTimeFactor'   :'',\
-    'FactorForSofterEQS'            :'',\
-    'SofteningGas'                  :'',\
-    'SofteningHalo'                 :'',\
-    'SofteningDisk'                 :'',\
-    'SofteningBulge'                :'',\
-    'SofteningStars'                :'',\
-    'SofteningBndry'                :'',\
-    'InitCondFile'                  :'achInFile',\
-    'SnapshotFileBase'              :'achOutFile',\
-    'EnergyFile'                    :'',\
-    'InfoFile'                      :'',\
-    'TimingsFile'                   :'',\
-    'CpuFile'                       :'',\
-    'TimebinFile'                   :'',\
-    'RestartFile'                   :'',\
-    'OutputDir'                     :'',\
-    'TimeLimitCPU'                  :'iWallRunTime',\
-    'ResubmitOn'                    :'',\
-    'ResubmitCommand'               :'',\
-    'ICFormat'                      :'',\
-    'SnapFormat'                    :'',\
-    'NumFilesPerSnapshot'           :'',\
-    'NumFilesWrittenInParallel'     :'',\
-    'ComovingIntegrationOn'         :'bComove',\
-    'CoolingOn'                     :'bGasCooling',\
-    'StarformationOn'               :'bStarForm',\
-    'TimeBegin'                     :'iStartStep',\
-    'TimeMax'                       :'',\
-    'Omega0'                        :'dOmega0',\
-    'OmegaLambda'                   :'dLambda',\
-    'OmegaBaryon'                   :'dOmegab',\
-    'HubbleParam'                   :'dHubble0',\
-    'BoxSize'                       :'dPeriod',\
-    'PeriodicBoundariesOn'          :'bPeriodic',\
-    'OutputListOn'                  :'',\
-    'OutputListFilename'            :'',\
-    'TimeBetSnapshot'               :'iOutInterval',\
-    'TimeOfFirstSnapshot'           :'',\
-    'CpuTimeBetRestartFile'         :'',\
-    'TimeBetStatistics'             :'iLogInterval',\
-    'TypeOfTimestepCriterion'       :'',\
-    'ErrTolIntAccuracy'             :'dEta',\
-    'MaxSizeTimestep'               :'dDelta',\
-    'MinSizeTimestep'               :'',\
-    'MaxRMSDisplacementFac'         :'',\
-    'ErrTolTheta'                   :'dTheta',\
-    'TypeOfOpeningCriterion'        :'',\
-    'ErrTolForceAcc'                :'',\
-    'TreeDomainUpdateFrequency'     :'',\
-    'ArtBulkViscConst'              :'dConstAlpha',\
-    'InitGasTemp'                   :'',\
-    'MinGasTemp'                    :'',\
-    'CourantFac'                    :'dEtaCourant',\
-    'DesNumNgb'                     :'nSmooth',\
-    'MaxNumNgbDeviation'            :'',\
-    'PartAllocFactor'               :'',\
-    'BufferSize'                    :'',\
-    'MaxMemSize'                    :'',\
-    'UnitLength_in_cm'              :'dKpcUnit',\
-    'UnitMass_in_g'                 :'dMsolUnit',\
-    'UnitVelocity_in_cm_per_s'      :'',\
-    'GravityConstantInternal'       :'',\
-    'MinGasHsmlFractional'          :'dhMinOverSoft',\
-    'SofteningGasMaxPhys'           :'',\
-    'SofteningBulgeMaxPhys'         :'',\
-    'SofteningStarsMaxPhys'         :'',\
-    'SofteningBndryMaxPhys'         :'',\
-    'SofteningHaloMaxPhys'          :'',\
-    'SofteningDiskMaxPhys'          :'',\
-        }
+g2c_name_dict['CritPhysDensity'               ] = ''
+g2c_name_dict['MaxSfrTimescale'               ] = ''
+g2c_name_dict['CritOverDensity'               ] = ''
+g2c_name_dict['TempSupernova'                 ] = ''
+g2c_name_dict['TempClouds'                    ] = ''
+g2c_name_dict['FactorSN'                      ] = ''
+g2c_name_dict['FactorEVP'                     ] = ''
+g2c_name_dict['WindEfficiency'                ] = ''
+g2c_name_dict['WindEnergyFraction'            ] = ''
+g2c_name_dict['WindFreeTravelDensFac'         ] = ''
+g2c_name_dict['WindFreeTravelMaxTimeFactor'   ] = ''
+g2c_name_dict['FactorForSofterEQS'            ] = ''
+g2c_name_dict['SofteningGas'                  ] = ''
+g2c_name_dict['SofteningHalo'                 ] = ''
+g2c_name_dict['SofteningDisk'                 ] = ''
+g2c_name_dict['SofteningBulge'                ] = ''
+g2c_name_dict['SofteningStars'                ] = ''
+g2c_name_dict['SofteningBndry'                ] = ''
+g2c_name_dict['InitCondFile'                  ] = 'achInFile'
+g2c_name_dict['SnapshotFileBase'              ] = 'achOutFile'
+g2c_name_dict['EnergyFile'                    ] = ''
+g2c_name_dict['InfoFile'                      ] = ''
+g2c_name_dict['TimingsFile'                   ] = ''
+g2c_name_dict['CpuFile'                       ] = ''
+g2c_name_dict['TimebinFile'                   ] = ''
+g2c_name_dict['RestartFile'                   ] = ''
+g2c_name_dict['OutputDir'                     ] = ''
+g2c_name_dict['TimeLimitCPU'                  ] = 'iWallRunTime'
+g2c_name_dict['ResubmitOn'                    ] = ''
+g2c_name_dict['ResubmitCommand'               ] = ''
+g2c_name_dict['ICFormat'                      ] = ''
+g2c_name_dict['SnapFormat'                    ] = ''
+g2c_name_dict['NumFilesPerSnapshot'           ] = ''
+g2c_name_dict['NumFilesWrittenInParallel'     ] = ''
+g2c_name_dict['ComovingIntegrationOn'         ] = 'bComove'
+g2c_name_dict['CoolingOn'                     ] = 'bGasCooling'
+g2c_name_dict['StarformationOn'               ] = 'bStarForm'
+g2c_name_dict['TimeBegin'                     ] = 'iStartStep'
+g2c_name_dict['TimeMax'                       ] = 'nSteps'
+g2c_name_dict['Omega0'                        ] = 'dOmega0'
+g2c_name_dict['OmegaLambda'                   ] = 'dLambda'
+g2c_name_dict['OmegaBaryon'                   ] = 'dOmegab'
+g2c_name_dict['HubbleParam'                   ] = 'dHubble0'
+g2c_name_dict['BoxSize'                       ] = 'dPeriod'
+g2c_name_dict['PeriodicBoundariesOn'          ] = 'bPeriodic'
+g2c_name_dict['OutputListOn'                  ] = ''
+g2c_name_dict['OutputListFilename'            ] = ''
+g2c_name_dict['TimeBetSnapshot'               ] = 'iOutInterval'
+g2c_name_dict['TimeOfFirstSnapshot'           ] = ''
+g2c_name_dict['CpuTimeBetRestartFile'         ] = ''
+g2c_name_dict['TimeBetStatistics'             ] = 'iLogInterval'
+g2c_name_dict['TypeOfTimestepCriterion'       ] = ''
+g2c_name_dict['ErrTolIntAccuracy'             ] = 'dEta'
+g2c_name_dict['MaxSizeTimestep'               ] = 'dDelta'
+g2c_name_dict['MinSizeTimestep'               ] = ''
+g2c_name_dict['MaxRMSDisplacementFac'         ] = ''
+g2c_name_dict['ErrTolTheta'                   ] = 'dTheta'
+g2c_name_dict['TypeOfOpeningCriterion'        ] = ''
+g2c_name_dict['ErrTolForceAcc'                ] = ''
+g2c_name_dict['TreeDomainUpdateFrequency'     ] = ''
+g2c_name_dict['ArtBulkViscConst'              ] = 'dConstAlpha'
+g2c_name_dict['InitGasTemp'                   ] = ''
+g2c_name_dict['MinGasTemp'                    ] = ''
+g2c_name_dict['CourantFac'                    ] = 'dEtaCourant'
+g2c_name_dict['DesNumNgb'                     ] = 'nSmooth'
+g2c_name_dict['MaxNumNgbDeviation'            ] = ''
+g2c_name_dict['PartAllocFactor'               ] = ''
+g2c_name_dict['BufferSize'                    ] = ''
+g2c_name_dict['MaxMemSize'                    ] = ''
+g2c_name_dict['UnitLength_in_cm'              ] = 'dKpcUnit'
+g2c_name_dict['UnitMass_in_g'                 ] = 'dMsolUnit'
+g2c_name_dict['UnitVelocity_in_cm_per_s'      ] = ''
+g2c_name_dict['GravityConstantInternal'       ] = ''
+g2c_name_dict['MinGasHsmlFractional'          ] = 'dhMinOverSoft'
+g2c_name_dict['SofteningGasMaxPhys'           ] = ''
+g2c_name_dict['SofteningBulgeMaxPhys'         ] = ''
+g2c_name_dict['SofteningStarsMaxPhys'         ] = ''
+g2c_name_dict['SofteningBndryMaxPhys'         ] = ''
+g2c_name_dict['SofteningHaloMaxPhys'          ] = ''
+g2c_name_dict['SofteningDiskMaxPhys'          ] = ''
 

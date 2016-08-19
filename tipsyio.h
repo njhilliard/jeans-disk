1e52152627b9327899a24ce83b3be6cb1fd6c6f2
Tim Haines <thaines@astro.wisc.edu>
2016-08-19 02:47:03 -0500

    Import tipsy particle definitions

--- tipsyio.h
+++ tipsyio.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdio.h>
+#include "tipsy.h"
 
 // Convention: Positive are system errors, negative are Tipsy errors
 typedef enum {
@@ -17,73 +18,14 @@ FILE 	  * tipsy_get_fd();
 const char* tipsy_get_last_system_error();
 const char* tipsy_strerror(tipsy_error_t);
 
-int tipsy_read_header(double *time, int *ngas, int *ndark, int *nstar);
+int tipsy_read_header(tipsy_header*);
+int tipsy_read_star_particles(tipsy_star_data*);
+int tipsy_read_dark_particles(tipsy_dark_data*);
+int tipsy_read_gas_particles(tipsy_gas_data*);
+int tipsy_read_blackhole_particles(tipsy_blackhole_data*);
 
-int tipsy_read_star_particles(float *mass,
-			      float (*pos)[3],
-			      float (*vel)[3],
-			      float *metals,
-			      float *tform,
-			      float *phi,
-			      size_t size);
-
-int tipsy_read_dark_particles(float *mass,
-				float (*pos)[3],
-				float (*vel)[3],
-				float *phi,
-				size_t size);
-
-int tipsy_read_gas_particles(float *mass,
-			     float (*pos)[3],
-			     float (*vel)[3],
-			     float *rho,
-			     float *temp,
-			     float *hsmooth,
-			     float *metals,
-			     float *phi,
-			     size_t size);
-
-int tipsy_read_blackhole_particles(float *mass,
-				 float (*pos)[3],
-				 float (*vel)[3],
-				 float *phi,
-				 size_t size);
-
-int tipsy_write_header(double time, int ngas, int ndark, int nstar);
-
-int tipsy_write_star_particles(const float *mass,
-			       const float (*pos)[3],
-			       const float (*vel)[3],
-			       const float *metals,
-			       const float *tform,
-			       float	softening,
-			       float	mass_scale,
-			       float	velocity_scale,
-			       size_t   size);
-
-int tipsy_write_dark_particles(const float *mass,
-			       const float (*pos)[3],
-			       const float (*vel)[3],
-			       float  softening,
-			       float  mass_scale,
-			       float  velocity_scale,
-			       size_t size);
-
-int tipsy_write_gas_particles(const float *mass,
-			      const float (*pos)[3],
-			      const float (*vel)[3],
-			      const float *rho,
-			      const float *temp,
-			      const float *hsmooth,
-			      const float *metals,
-			      float	mass_scale,
-			      float	velocity_scale,
-			      size_t       size);
-
-int tipsy_write_blackhole_particles(const float *mass,
-				    const float (*pos)[3],
-				    const float (*vel)[3],
-				    const float softening,
-				    float       mass_scale,
-				    float       velocity_scale,
-				    size_t      size);
+int tipsy_write_header(tipsy_header const*);
+int tipsy_write_star_particles(tipsy_star_data const*);
+int tipsy_write_dark_particles(tipsy_dark_data const*);
+int tipsy_write_gas_particles(tipsy_gas_data const*);
+int tipsy_write_blackhole_particles(tipsy_blackhole_data const*);

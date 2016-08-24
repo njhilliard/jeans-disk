13cd1cda38474c411d590ca773a07f7fc7efe85a
Tim Haines <thaines@astro.wisc.edu>
2016-08-23 19:02:13 -0500

    Move gadget_converter to driver file; many bug fixes

--- gadget2changa.py
+++ gadget2changa.py
@@ -5,6 +5,43 @@ import ChaNGa
 import tipsy
 import argparse
 import os
+import astropy.units as apu
+import astropy.constants as apc
+import math
+import numpy as np
+
+def convert_U_to_temperature(gadget_params, gadget_file):
+    # TODO: Check these units wrt the mass conversion between GADGET and ChaNGa
+    units = {}
+    units['Length_in_cm'] = float(gadget_params['UnitLength_in_cm']) / hubble
+    units['Mass_in_g'] = float(gadget_params['UnitMass_in_g']) / hubble
+    units['Velocity_in_cm_per_s'] = float(gadget_params['UnitVelocity_in_cm_per_s'])
+    units['Time_in_s'] = units['Length_in_cm'] / units['Velocity_in_cm_per_s']
+    units['Energy_in_cgs'] = units['Mass_in_g'] * units['Length_in_cm'] ** 2 / units['Time_in_s'] ** 2
+    
+    constants = {
+        'boltzmann'     : apc.k_B.cgs.value,
+        'protonmass'    : apc.m_p.cgs.value,
+        'gamma_minus1'  : (5.0 / 3.0) - 1.0,
+        'h_massfrac'    : 0.76
+    }
+    
+    # Convert temperature to Kelvin
+    print('Converting internal energy to temperature assuming a neutral hydrogen-only gamma=5/3 gas and non-traditional SPH')
+    
+    # See GADGET3/density.c:1426  for details on these calculations
+    mean_weight = 4.0 / (1.0 + 3.0 * constants['h_massfrac'])
+    
+    if gadget_file.gas.metals is not None and gadget_file.gas.electron_density is not None:
+        X = gadget_file.gas.metals / gadget_file.gas.mass
+        mask = X > 0.0
+        Y = np.zeros(X.shape)
+        Y[mask] = (1.0 - X[mask]) / (4.0 * X[mask])
+        mean_weight = (1.0 + 4.0 * Y) / (1.0 + Y + gadget_file.gas.electron_density)
+    
+    gas_temp = constants['gamma_minus1'] / constants['boltzmann'] * gadget_file.gas.internal_energy / gadget_file.gas.mass
+    gas_temp *= constants['protonmass'] * mean_weight * units['Energy_in_cgs'] / units['Mass_in_g']
+    return gas_temp
 
 def get_output_file(file_name):
     input_dir, input_name = os.path.split(file_name)
@@ -23,24 +60,117 @@ parser.add_argument('--preserve-boundary-softening', action='store_true', help='
 parser.add_argument('--no-param-list', action='store_true', help='Do not store a complete list ChaNGa parameters in "param_file"')
 args = parser.parse_args()
 
-output_file = get_output_file(args.gadget_file)
+gadget_params = gadget.Parameter_file(args.param_file)
+changa_params, mass_scale = ChaNGa.convert_parameter_file(gadget_params)
 
 gadget_file = gadget.File(args.gadget_file)
-gadget_params = gadget.Parameter_file(args.param_file)
-changa_params, mass_factor = ChaNGa.convert_parameter_file(gadget_params)
+changa_params['bDoGas'] = int(gadget_file.gas is not None)
 
-tipsy_file = tipsy.gadget_converter(gadget_params, gadget_file, mass_factor, args.convert_bh, args.preserve_boundary_softening)
+output_file = get_output_file(args.gadget_file)
 
-changa_params['bDoGas'] = int(tipsy_file.gas is not None)
-
-#Output the parameter file
+# Output the parameter file
 with open(output_file + '.ChaNGa.params', 'w') as f:
-     for k in sorted(changa_params):
+    for k in sorted(changa_params):
          f.write('{0:20s}{1:s}\n'.format(k, str(changa_params[k])))
 
-if not args.no_param_list:
-    with open(output_file + '.ChaNGa.params', 'a') as f:
+    if not args.no_param_list:
         f.write('\n# Complete parameter list below\n')
         f.write(ChaNGa.all_parameters)
 
-tipsy_file.save(output_file + '.tipsy')
+##################################################################################
+time = float(gadget_file.header['Time'])
+is_cosmological = int(gadget_params['ComovingIntegrationOn']) == 1
+
+# Gadget units have an extra sqrt(a) in the internal velocities
+velocity_scale = math.sqrt(1.0 + float(gadget_file.header['Redshift']))
+
+hubble = 1.0
+if is_cosmological:
+    hubble = float(gadget_params['HubbleParam'])
+    if hubble == 0.0:
+        hubble = 1.0
+
+with tipsy.streaming_writer(output_file + '.tipsy') as file:
+    ngas = ndark = nstar = 0
+    
+    # just a placeholder
+    file.header(time, ngas, ndark, nstar)
+    
+    if gadget_file.gas is not None:
+        gas = gadget_file.gas
+        ngas += gas.size
+        gas_temp = convert_U_to_temperature(gadget_params, gadget_file)
+        gas.mass *= mass_scale
+        gas.velocities *= velocity_scale
+        file.gas(gas.mass, gas.positions, gas.velocities, gas.density, gas_temp, gas.hsml, gas.metals, gas.potential, gas.size)
+    
+    if gadget_file.halo is not None:
+        halo = gadget_file.halo
+        ndark += halo.size
+        halo.mass *= mass_scale
+        halo.velocities *= velocity_scale
+        file.darkmatter(halo.mass, halo.positions, halo.velocities, halo.potential, gadget_params['SofteningHalo'], halo.size)
+        
+    # In ChaNGa, cosmological simulations treat disk and bulge particles
+    # as dark matter particles
+    if is_cosmological:
+        if gadget_file.disk is not None:
+            disk = gadget_file.disk
+            ndark += disk.size
+            disk.mass *= mass_scale
+            disk.velocities *= velocity_scale
+            file.darkmatter(disk.mass, disk.positions, disk.velocities, disk.potential, gadget_params['SofteningDisk'], disk.size)
+        
+        if gadget_file.bulge is not None:
+            bulge = gadget_file.bulge
+            ndark += bulge.size
+            bulge.mass *= mass_scale
+            bulge.velocities *= velocity_scale
+            file.darkmatter(bulge.mass, bulge.positions, bulge.velocities, bulge.potential, gadget_params['SofteningBulge'], bulge.size)
+    
+    # Convert boundary particles to dark matter particles
+    if gadget_file.boundary is not None and not convert_bh:
+        boundary = gadget_file.boundary
+        ndark += boundary.size
+        boundary.mass *= mass_scale
+        boundary.velocities *= velocity_scale
+        eps = gadget_params['SofteningBndry'] if args.preserve_boundary_softening else gadget_params['SofteningHalo']
+        file.darkmatter(boundary.mass, boundary.positions, boundary.velocities, boundary.potential, eps, boundary.size)
+        
+    if not is_cosmological:
+        if gadget_file.disk is not None:
+            disk = gadget_file.disk
+            nstar += disk.size
+            disk.mass *= mass_scale
+            disk.velocities *= velocity_scale
+            file.stars(disk.mass, disk.positions, disk.velocities, None, None, disk.potential,
+                      gadget_params['SofteningDisk'], disk.size)
+        
+        if gadget_file.bulge is not None:
+            bulge = gadget_file.bulge
+            nstar += bulge.size
+            bulge.mass *= mass_scale
+            bulge.velocities *= velocity_scale
+            file.stars(bulge.mass, bulge.positions, bulge.velocities, bulge.metals, bulge.t_form, bulge.potential,
+                      gadget_params['SofteningBulge'], bulge.size)
+
+    if gadget_file.stars is not None:
+        star = gadget_file.stars
+        nstar += star.size
+        star.mass *= mass_scale
+        star.velocities *= velocity_scale
+        file.stars(star.mass, star.positions, star.velocities, star.metals, star.t_form, star.potential,
+                   gadget_params['SofteningStars'], star.size)
+    
+    # Convert boundary particles to black holes
+    if gadget_file.boundary is not None and convert_bh:
+        boundary = gadget_file.boundary
+        nstar += boundary.size
+        boundary.mass *= mass_scale
+        boundary.velocities *= velocity_scale
+        file.stars(boundary.mass, boundary.positions, boundary.velocities, None, None, boundary.potential,
+                   gadget_params['SofteningBndry'], boundary.size, is_blackhole=True)
+
+# update the header
+with tipsy.streaming_writer(output_file + '.tipsy', 'r+b') as file:
+    file.header(time, ngas, ndark, nstar)
--- tipsy.py
+++ tipsy.py
@@ -1,7 +1,4 @@
 import gadget
-import astropy.units as apu
-import astropy.constants as apc
-import math
 import numpy as np
 from tipsy_c import *
 
@@ -67,157 +64,33 @@ class File():
             self.gas_particles = tipsy_gas_data(self.hdr.ngas)
             self.lib.tipsy_read_gas_particles(ctypes.byref(self.gas_particles))
         return self.gas_particles
+
+class streaming_writer():
+    def __init__(self, filename, mode='wb'):
+        self.lib = load_tipsy()
+        self.lib.tipsy_open_file(ctypes.c_char_p(bytes(filename, 'utf-8')),
+                                 ctypes.c_char_p(bytes(mode, 'utf-8')))
     
-    def save(self):
-        self.lib.tipsy_write_header(time, ngas, ndark, nstar)
-        self.lib.tipsy_write_dark_particles(mass, pos, vel, softening, mass.shape[0])
-        self.lib.tipsy_write_star_particles(mass, pos, vel, metals, t_form, softening, mass.shape[0])
-        self.lib.tipsy_write_gas_particles(mass, pos, vel, density, temperature, hsmooth, metals, mass.shape[0])
-        
-class gadget_converter():
-    # Some useful astronomy constants in cgs units
-    constants = {
-        'gravity'       : apc.G.cgs.value,
-        'boltzmann'     : apc.k_B.cgs.value,
-        'protonmass'    : apc.m_p.cgs.value,
-        'sec_per_myr'   : apu.yr.to(apu.s) * 1e6,
-        'gamma_minus1'  : (5.0 / 3.0) - 1.0,
-        'h_massfrac'    : 0.76
-    }
+    def header(self, time, ngas, ndark, nstars):
+        self.lib.tipsy_write_header(time, ngas, ndark, nstars)
 
-    def __init__(self, gadget_params, gadget_file, mass_conversion_factor, convert_bh=False, preserve_bndry_softening=False):
-        if not isinstance(gadget_file, gadget.File):
-            raise TypeError("input file must be a gadget.File")
-        
-        if not isinstance(gadget_params, gadget.Parameter_file):
-            raise TypeError("input file must be a gadget.Parameter_file")
+    def gas(self, mass, pos, vel, rho, temp, hsmooth, metals, phi, size):
+        self.lib.tipsy_write_gas_particles(mass, pos, vel, rho, temp, hsmooth, metals, phi, size)
+    
+    def darkmatter(self, mass, pos, vel, phi, softening, size):
+        softening = np.asscalar(np.array(softening, dtype=np.float32))
+        self.lib.tipsy_write_dark_particles(mass, pos, vel, phi, softening, size)    
+    
+    def stars(self, mass, pos, vel, metals, tform, phi, softening, size, is_blackhole=False):
+        softening = np.asscalar(np.array(softening, dtype=np.float32))
+        self.lib.tipsy_write_star_particles(mass, pos, vel, metals, tform, phi, softening, size, is_blackhole)
 
-        self.convert_bh = convert_bh
-        self.preserve_bndry_softening = preserve_bndry_softening
-        self.mass_scale = float(mass_conversion_factor)
-        self.time = float(gadget_file.header['Time'])
-        self.redshift = float(gadget_file.header['Redshift'])
-        self.is_cosmological = int(gadget_params['ComovingIntegrationOn']) == 1
-        
-        self.halo = None
-        if gadget_file.halo is not None:
-            self.halo = gadget_file.halo
-            self.halo.softening = float(gadget_params['SofteningHalo'])
-        
-        self.disk = None
-        if gadget_file.disk is not None:
-            self.disk = gadget_file.disk
-            self.disk.softening = float(gadget_params['SofteningDisk'])
-        
-        self.bulge = None
-        if gadget_file.bulge is not None:
-            self.bulge = gadget_file.bulge
-            self.bulge.softening = float(gadget_params['SofteningBulge'])
-        
-        self.star = None
-        if gadget_file.star is not None:
-            self.star = gadget_file.star
-            self.star.softening = float(gadget_params['SofteningStars'])
-        
-        self.boundary = None
-        if gadget_file.boundary is not None:
-            self.boundary = gadget_file.boundary
-            self.boundary.softening = float(gadget_params['SofteningBndry'])
+    def close(self):
+        self.lib.tipsy_close_file()
 
-        self.gas = None
-        if gadget_file.gas is not None:
-            self.gas = gadget_file.gas
-            self.gas.softening = gadget_params['SofteningGas']
-            
-            # Convert temperature to Kelvin
-            print('Converting internal energy to temperature assuming a gamma=5/3 gas')
-            mean_weight = (4.0 / 
-                           (3 * constants['h_massfrac'] + 1 + 
-                            4 * constants['h_massfrac'] * self.gas.electron_density
-                           ) * constants['protonmass'])
-            
-            hubble = 1.0
-            if int(gadget_params['ComovingIntegrationOn']) == 1:
-                hubble = float(gadget_params['HubbleParam'])
-                if hubble == 0.0:
-                    hubble = 1.0
-            
-            # TODO: Check these units wrt the mass conversion between GADGET and ChaNGa
-            units['Length_in_cm'] = float(gadget_params['UnitLength_in_cm']) / hubble
-            units['Mass_in_g'] = float(gadget_params['UnitMass_in_g']) / hubble
-            units['Velocity_in_cm_per_s'] = float(gadget_params['UnitVelocity_in_cm_per_s'])
-            units['Time_in_s'] = units['Length_in_cm'] / units['Velocity_in_cm_per_s']
-            units['Energy_in_cgs'] = units['Mass_in_g'] * units['Length_in_cm'] ** 2 / units['Time_in_s'] ** 2
-            
-            self.gas.temperature = gadget_file.gas.internal_energy * \
-                                   (mean_weight / constants['boltzmann'] * 
-                                    constants['gamma_minus1'] * 
-                                    units['Energy_in_cgs'] / units['Mass_in_g'])
-
-    def save(self, filename):
-        ngas, ndark, nstar = 0, 0, 0
-        
-        with tipsy_file.streaming_writer(filename) as file:
-            # Gadget units have an extra sqrt(a) in the internal velocities
-            file.set_velocity_scale(math.sqrt(1.0 + self.redshift))
-            
-            file.set_mass_scale(self.mass_scale)
-            
-            # just a placeholder
-            file.write_header(self.time, ngas, ndark, nstar)
-            
-            if self.halo is not None:
-                ndark += self.halo.size
-                file.write_dark(self.halo.mass, self.halo.positions,
-                                self.halo.velocities, self.halo.softening)
-                
-            # In ChaNGa, cosmological simulations treat disk and bulge particles
-            # as dark matter particles
-            if self.is_cosmological:
-                if self.disk is not None:
-                    ndark += self.disk.size
-                    file.write_dark(self.disk.mass, self.disk.positions,
-                                    self.disk.velocities, self.disk.softening)
-                
-                if self.bulge is not None:
-                    ndark += self.bulge.size
-                    file.write_dark(self.bulge.mass, self.bulge.positions,
-                                    self.bulge.velocities, self.bulge.softening)
-            
-            # Convert boundary particles to dark matter particles
-            if self.boundary is not None and not self.convert_bh:
-                ndark += self.boundary.size
-                eps = self.halo.softening
-                if not self.preserve_bndry_softening:
-                    eps = self.boundary.softening
-                file.write_dark(self.boundary.mass, self.boundary.positions,
-                                self.boundary.velocities, eps)
-                
-            if not self.is_cosmological:
-                if self.disk is not None:
-                    nstar += self.disk.size
-                    file.write_star(self.disk.mass, self.disk.positions,
-                                    self.disk.velocities, self.disk.metals,
-                                    self.disk.t_form, self.disk.softening)
-                
-                if self.bulge is not None:
-                    nstar += self.bulge.size
-                    file.write_star(self.bulge.mass, self.bulge.positions,
-                                    self.bulge.velocities, self.bulge.softening)
-
-
-            if self.star is not None:
-                nstar += self.star.size
-                file.write_star(self.star.mass, self.star.positions,
-                                self.star.velocities, self.star.metals,
-                                self.star.t_form, self.star.softening)
-            
-            # Convert boundary particles to black holes
-            if self.boundary is not None and self.convert_bh:
-                nstar += self.boundary.size
-                file.write_star(self.boundary.mass, self.boundary.positions,
-                                self.boundary.velocities, self.boundary.softening)
-       
-        # update the header
-        with tipsy_file.streaming_writer(filename, 'r+b') as file:
-            file.write_header(self.time, ngas, ndark, nstar)
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+        return False  # always re-raise exceptions
